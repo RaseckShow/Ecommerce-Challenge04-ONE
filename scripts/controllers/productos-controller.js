33d4666390bb6c86c113d796d825769c07b940bc
RaseckShow <ces_art11@hotmail.com>
2022-12-09 22:05:36 -0600

    implementacion muestra de productos automatica

---
+++ scripts/controllers/productos-controller.js
@@ -0,0 +1,46 @@
+import { productoServices } from "../servicios/productos-servicios.js";
+
+const nuevoProducto = (name, price, imageUrl) => {
+    const card = document.createElement("div");
+    const content = 
+    `<div class="producto__box">
+    <img src="${imageUrl}" alt="" class="producto__img">
+    <h3 class="producto__name">${name}</h3>
+    <p class="producto__precio">${price}</p>
+    <a href=""><button class="verproducto__btn">Ver Producto</button></a>
+    </div>`
+
+    card.innerHTML = content;
+    card.classList.add("card");
+    return (card);
+}
+
+const producto = document.querySelectorAll("[data-productos]");
+
+const render = async() => {
+    try{
+        console.log(producto);
+        const listaProductos = await productoServices.listaProductos();
+        listaProductos.forEach(elemento => {
+            const categoria = elemento.category;
+            switch(categoria){
+                case "star wars":
+                    producto[0].appendChild(nuevoProducto(elemento.name, elemento.price,elemento.imageUrl));
+                break;
+                case "consolas":
+                    producto[1].appendChild(nuevoProducto(elemento.name, elemento.price,elemento.imageUrl));
+                break;
+                case "diversos":
+                    producto[2].appendChild(nuevoProducto(elemento.name, elemento.price,elemento.imageUrl));
+                break;
+                    
+            }
+            // producto.appendChild(nuevoProducto(elemento.name, elemento.price,elemento.imageUrl))
+        });
+    }
+    catch(error){
+        console.log(error);
+    }
+}
+
+render();
