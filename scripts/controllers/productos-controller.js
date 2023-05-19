b0ca4be62cd78b46ecbe2a3b16cd49d98afe7823
RaseckShow <ces_art11@hotmail.com>
2023-05-18 21:43:53 -0600

    Mejora de responsividad

--- scripts/controllers/productos-controller.js
+++ scripts/controllers/productos-controller.js
@@ -4,7 +4,7 @@ const nuevoProducto = (name, price, imageUrl,id) => {
     const card = document.createElement("div");
     const content = 
     `<div class="producto__box">
-    <img src="${imageUrl}" alt="" class="producto__img">
+    <img src="${imageUrl}" alt="" class="producto__img" >
     <h3 class="producto__name">${name}</h3>
     <p class="producto__precio">$ ${price}</p>
     <a href="descripcionProducto.html"><button id="${id}" class="verproducto__btn">Ver Producto</button></a>
@@ -14,25 +14,53 @@ const nuevoProducto = (name, price, imageUrl,id) => {
     card.classList.add("card");
     return (card);
 }
-
+const productosAMostrar =(width)=>{
+    let cantidadProductos= 0;
+    switch (true) {
+        case (width < 768):
+            cantidadProductos = 4;
+            break;
+        case (width < 1024 && width>=768):
+            cantidadProductos = 5;
+        break;
+        default:
+            cantidadProductos = 6;
+        break;
+    }
+    return cantidadProductos;
+}
 const producto = document.querySelectorAll("[data-productos]");
 
 const render = async() => {
     try{
+        const width = document.documentElement.clientWidth;
+        
         const listaProductos = await productoServices.listaProductos();
+        let productosMostradosStarwars = 0;
+        let productosMostradosConsolas = 0;
+        let productosMostradosDiversos = 0;
         listaProductos.forEach(elemento => {
             const categoria = elemento.category;
+
             switch(categoria){
                 case "star wars":
-                    producto[0].appendChild(nuevoProducto(elemento.name, elemento.price,elemento.imageUrl,elemento.id));
+                    if (productosMostradosStarwars < productosAMostrar(width)) {
+                        producto[0].appendChild(nuevoProducto(elemento.name, elemento.price,elemento.imageUrl,elemento.id));
+                        productosMostradosStarwars++;                        
+                    } 
                 break;
                 case "consolas":
-                    producto[1].appendChild(nuevoProducto(elemento.name, elemento.price,elemento.imageUrl,elemento.id));
+                    if (productosMostradosConsolas < productosAMostrar(width)) {
+                        producto[1].appendChild(nuevoProducto(elemento.name, elemento.price,elemento.imageUrl,elemento.id));
+                        productosMostradosConsolas++;                        
+                    }     
                 break;
                 case "diversos":
-                    producto[2].appendChild(nuevoProducto(elemento.name, elemento.price,elemento.imageUrl,elemento.id));
+                    if (productosMostradosDiversos < productosAMostrar(width)) {
+                        producto[2].appendChild(nuevoProducto(elemento.name, elemento.price,elemento.imageUrl,elemento.id));                        
+                        productosMostradosDiversos++;                        
+                    } 
                 break;
-                    
             }
         });
     }
